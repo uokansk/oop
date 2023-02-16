6b71b39468973254c3acac82ede409ce6820c9af
Aleksandr <uorkansk@yandex.ru>
2023-02-16 22:54:23 +0700

    добавил описание

--- src/main/java/oopSeminar1/Main.java
+++ src/main/java/oopSeminar1/Main.java
@@ -1,5 +1,7 @@
 package oopSeminar1;
-
+/*
+* На основе кода с урока добавить еще один класс продукта, (Молоко, шоколад ,и т.п., можно добавить несколько классов), они должны наследоваться от класса Product, надо переопределить метод toString и попробовать включить эти классы в VendingMachine, вывести список товаров из VendingMachine
+* */
 
 public class Main {
     public static void main(String[] args) {
